976ce22176ec50705279fa7bbfc730b9e943f48e
Tim <tim.salokat@outlook.com>
2022-06-09 22:37:19 +0200

    Created Laptop Enviroment

--- Todo/frontend/src/App.js
+++ Todo/frontend/src/App.js
@@ -1,5 +1,5 @@
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlinePlusCircle } from "react-icons/ai"
 
 import SideBar from "./components/SideBar";
